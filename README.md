5847cf2b5fb5aae7c930e774314ad6edbde92913
Ruwan Abeykoon <ruwana@wso2.com>
2018-01-14 12:38:31 +0530

    Adding initial PoC

---
+++ README.md
@@ -0,0 +1,21 @@
+# Proof Of Concept MSF4J can be embedded in traditional Web-APP
+
+
+## usage
+
+Add the following to the web.xml
+```XML
+    <servlet>
+        <servlet-name>msf4j</servlet-name>
+        <servlet-class>com.wso2.carbon.identity.msf4j.tomcat.Msf4jBridgeServlet</servlet-class>
+        <init-param>
+            <param-name>ServiceClasses</param-name>
+            <!-- Comma sepereted service classes -->
+            <param-value>com.wso2.carbon.identity.msf4j.tomcat.HelloService</param-value>
+        </init-param>
+    </servlet>
+    <servlet-mapping>
+        <servlet-name>msf4j</servlet-name>
+        <url-pattern>/*</url-pattern>
+    </servlet-mapping>
+```
---
+++ servlet/src/main/java/com/wso2/carbon/identity/msf4j/tomcat/HelloService.java
@@ -0,0 +1,37 @@
+package com.wso2.carbon.identity.msf4j.tomcat;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloService {
+
+    private Map<String, User> nameToUserMap = new HashMap<>();
+
+    @GET
+    @Path("/user/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(@PathParam("name") String name) {
+        User result = nameToUserMap.get(name);
+        if (result != null) {
+            return result;
+        }
+        return null;
+    }
+
+    @POST
+    @Path("/user")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloPost(User user) {
+        nameToUserMap.put(user.getName(), user);
+        return user.getName();
+    }
+}
